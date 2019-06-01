5c53e35644dd980dc4f066544b23efb7880f6669
anchor <anchor@anchordeMacBook-Pro.local>
2019-06-01 17:29:28 -0400

    First git init

---
+++ nodejs/blog-1/app.js
@@ -0,0 +1,7 @@
+const serverHandle = (req, res) => {
+    //  設置返回格式 JSON
+    res.setHeader('Content-type', 'application/json');
+
+}
+
+module.exports = serverHandle;
---
+++ nodejs/index.js
@@ -0,0 +1,6 @@
+var v = 'hi';
+var a = 6;
+var b = 7;
+console.log(v);
+console.log(a+b);
+console.log('show log');
