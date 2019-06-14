403c4c7a37f2301862e747220b6eb84101cd8ea4
anchor810321 <anchor810321@gmail.com>
2019-06-13 22:58:17 -0400

    Add handleBlogRouter

--- nodejs/blog-1/app.js
+++ nodejs/blog-1/app.js
@@ -2,6 +2,8 @@ const serverHandle = (req, res) => {
     //  設置返回格式 JSON
     res.setHeader('Content-type', 'application/json');
 
+    
+
 }
 
 module.exports = serverHandle;
---
+++ nodejs/blog-1/src/router/blog.js
@@ -0,0 +1,42 @@
+const handleBlogRouter = (req, res) =>{
+    const method = req.method; //GET POST
+    const url = req.url;
+    const path = url.split('?')[0];
+
+
+    //獲取博客
+    if(method === 'GET' && path === '/api/blog/list'){
+        return {
+            msg: 'Get Blog list interface'
+        }
+    };
+
+    //獲取博客詳情
+    if(method === 'GET' && path ==='/api/blog/detail'){
+        return {
+            msg: 'Get Blog detail interface'
+        }
+    };
+
+    //新建博客詳情
+    if(method === 'POST ' && path ==='/api/blog/new'){
+        return {
+            msg: 'This is new blog interface'
+        }
+    };
+
+    //更新博客詳情
+    if(method === 'POST ' && path ==='/api/blog/update'){
+        return {
+            msg: 'This is update blog interface'
+        }
+    };
+
+    //刪除博客詳情
+    if(method === 'POST ' && path ==='/api/blog/delete'){
+        return {
+            msg: 'This is delete blog interface'
+        }
+    };
+}
+module.exports = handleBlogRouter;
